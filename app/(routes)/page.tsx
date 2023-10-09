3ff1ffd91ca6e8ba113a578fcfb0021d9adf4b4d
Arjun Nahar <ArjunNahar@dunsterhouse.co.uk>
2023-10-09 17:15:54 +0100

    added new component

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -3,13 +3,15 @@ import Hero from '../components/Hero'
 import LinkGrid from '../components/LinkGrid'
 import StatsGrid from '../components/StatsGrid'
 import HeroBanner from '../components/HeroBanner'
+import PartnersGrid from '../components/PartnersGrid'
 
 export default function Home() {
   return (
     <>
     {/* <Hero title="An Agency Website Template" desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores laborum labore provident impedit esse recusandae facere libero harum sequi."/> */}
     <HeroBanner/>
-    <StatsGrid/>
+    {/* <StatsGrid/> */}
+    <PartnersGrid/>
     <LinkGrid title='Our Services' desc='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur
         aliquam doloribus nesciunt eos fugiat. Vitae aperiam fugit consequuntur
         saepe laborum.'/>
